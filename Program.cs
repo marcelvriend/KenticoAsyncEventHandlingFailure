e301e5593361d1042f41da67e878f45e92745ff6
Marcel Vriend <92307684+marcelvriend@users.noreply.github.com>
2023-03-03 12:00:44 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using CMS.Base;
+using CMS.DataEngine;
+using CMS.DocumentEngine;
+using Microsoft.Extensions.Hosting;
+
+Host.CreateDefaultBuilder()
+    .ConfigureServices((_, _) => { CMSApplication.Init(); })
+    .Build();
+
+using var context = new CMSActionContext();
+context.DisableAll();
+
+var foo = new Something();
+foo.SomeEvent += async void (_, _) =>
+{
+    async Task<List<TreeNode>> Retrieve() =>
+        (await new MultiDocumentQuery()
+            .WhereEquals("NodeID",
+                1) // Must be an existing NodeID, with a non-existing NodeID and culture it doesn't throw exceptions.
+            .Culture("en-US")
+            .Published()
+            .WithCoupledColumns()
+            .GetEnumerableTypedResultAsync().ConfigureAwait(false))
+        .ToList();
+
+    var result = await Retrieve();
+    Console.WriteLine(result.Count);
+};
+
+for (var i = 0; i < 10; i++)
+{
+    foo.DoSomething();
+}
+
+internal class Something
+{
+    public delegate void EventHandler(object sender, EventArgs e);
+
+    public event EventHandler? SomeEvent;
+
+    public void DoSomething()
+    {
+        SomeEvent?.Invoke(this, EventArgs.Empty);
+    }
+}
